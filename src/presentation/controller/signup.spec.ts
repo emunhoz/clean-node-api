30662f61bdab3cdde2bb83c66c3e7a61c4a40082
Eder Munhoz dos Santos <edermunhozsantos@gmail.com>
2020-03-24 22:49:20 -0300

    test: add SignupController returns 400

---
+++ src/presentation/controller/signup.spec.ts
@@ -0,0 +1,17 @@
+import { SignUpController } from './signup'
+
+describe('Signup Controller', () => {
+  test('return 400 if no name is provided ', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'John Doe',
+        email: 'johndoe@mail.com',
+        password: '123456',
+        passwordConfirmation: '123456'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+  })
+})
